175952c2762ed9d4d44e5c4546a2e97ed351e6dc
zuoning <16601037890@163.com>
2021-01-12 12:02:36 +0800

    基础搭建

---
+++ src/main/java/com/guigu/basis/JavaBasisApplication.java
@@ -0,0 +1,17 @@
+package com.guigu.basis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: java_basis
+ * @description
+ * @author: Zuo Ning
+ * @create: 2021-01-12 11:38
+ **/
+@SpringBootApplication
+public class JavaBasisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBasisApplication.class, args);
+    }
+}
