4b836716af72bfb3becb86ec7f911c92269d11ec
zuoning <zuoning@haochezhu.club>
2021-01-15 17:21:13 +0800

    BlockingQueue

---
+++ src/main/java/com/guigu/basis/JUC/queue/BlockingQueueDemo.java
@@ -0,0 +1,55 @@
+package com.guigu.basis.JUC.queue;
+
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program： com.guigu.basis.JUC.queue
+ * @description: 阻塞队列
+ * @author:
+ * @create: 2021-01-15 16:35
+ **/
+public class BlockingQueueDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<>(3);
+//        System.out.println(blockingQueue.add("a"));
+//        System.out.println(blockingQueue.add("b"));
+//        System.out.println(blockingQueue.add("c"));
+//
+//        System.out.println(blockingQueue.element());
+//
+//        System.out.println(blockingQueue.remove());
+//        System.out.println(blockingQueue.remove());
+//        System.out.println(blockingQueue.remove());
+
+//        System.out.println(blockingQueue.offer("a"));
+//        System.out.println(blockingQueue.offer("b"));
+//        System.out.println(blockingQueue.offer("c"));
+//        System.out.println(blockingQueue.offer("d"));
+//
+//        System.out.println(blockingQueue.peek());
+//
+//        System.out.println(blockingQueue.poll());
+//        System.out.println(blockingQueue.poll());
+//        System.out.println(blockingQueue.poll());
+//        System.out.println(blockingQueue.poll());
+
+//        blockingQueue.put("a");
+//        blockingQueue.put("a");
+//        blockingQueue.put("a");
+//        System.out.println("=====");
+//        blockingQueue.put("x");
+//        blockingQueue.take();
+//        blockingQueue.take();
+//        blockingQueue.take();
+//        blockingQueue.take();
+//        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+//        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+//        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+//        System.out.println(blockingQueue.offer("a",2L, TimeUnit.SECONDS));
+    }
+}
