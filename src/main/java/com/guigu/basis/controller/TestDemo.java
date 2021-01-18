0954c7564e3678fb0ea0aecbeaad8384b9ef83d6
16601 <16601037890@163.com>
2021-01-18 22:17:52 +0800

    完成

--- src/main/java/com/guigu/basis/controller/TestDemo.java
+++ src/main/java/com/guigu/basis/controller/TestDemo.java
@@ -15,43 +15,34 @@ import java.util.*;
 public class TestDemo {
 
     public static void main(String[] args) {
-//        int[] b = {3, 4, 6, 8, 10, 10, 11};
-        int[] b = { 10, 10, 11};
-//        List<Integer> list = new ArrayList<>();
-//        for (int i = 0; i < b.length; i++) {
-//            if (!list.contains(b[i])) {
-//                list.add(b[i]);
-//            }
-//        }
-//        System.out.println(list);
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int[] b = {3, 4, 4, 10, 10, 10, 11};
+        Map<Integer, Integer> map1 = new HashMap<Integer, Integer>();
         for (int i = 0; i < b.length; i++) {
-            int j = 1;
-            if (map.containsKey(b[i])) {
-                map.put(b[i], j= j++);
+            if (map1.containsKey(b[i])) {
+                map1.put(b[i], map1.get(b[i]) + 1);
             } else {
-                map.put(b[i], 1);
+                map1.put(b[i], 1);
             }
         }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue()%2>0){
+        for (Map.Entry<Integer, Integer> entry : map1.entrySet()) {
+            if (entry.getValue() % 2 > 0) {
                 System.out.println(entry.getKey());
             }
         }
 
 
-//        int[] arr = {3, 4, 6, 8, 10,10};
-//        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-//        for (int i = 0; i < arr.length; i++) {
-//            if (map.containsKey(arr[i])) {
-//                map.remove(arr[i]);//出现了偶数次则删掉
-//            } else {
-//                map.put(arr[i], 1);//还未出现过则次数设为1
-//            }
-//        }
-//        Iterator iterator = map.keySet().iterator();
-//        while (iterator.hasNext()) {
-//            System.out.println(iterator.next());
-//        }
+        int[] arr = {3, 4, 8, 8, 8, 10, 10};
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.remove(arr[i]);//出现了偶数次则删掉
+            } else {
+                map.put(arr[i], 1);//还未出现过则次数设为1
+            }
+        }
+        Iterator iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 }
